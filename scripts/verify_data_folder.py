5219cefb171fe8bd3cf07a5e3facb05f8e6e5782
Colin Kern <jckern@ucsd.edu>
2022-10-27 09:17:48 -0700

    Improved script

--- scripts/verify_data_folder.py
+++ scripts/verify_data_folder.py
@@ -1,42 +1,57 @@
 import sys
 import pathlib
 import re
-from collections import defaultdict
+from collections import namedtuple
+from tqdm import tqdm
+import pandas as pd
 
-folder = pathlib.Path(sys.argv[1])
-print(f"Verifying {folder}...")
+ImageFile = namedtuple("ImageFile", ["round", "fov", "size"])
 
-sizes = defaultdict(lambda: defaultdict(int))
 
-count = 0
-rounds = set()
-fovs = set()
-for dax in folder.glob("*.dax"):
-    size = dax.stat().st_size
-    match = re.search("_H([0-9]+)_F_?([0-9]+).dax", str(dax))
-    round = int(match.group(1))
-    fov = int(match.group(2))
-    sizes[round][size] += 1
-    count += 1
-    rounds.add(round)
-    fovs.add(fov)
+def verify_folder(folder):
+    files = []
+    for dax in tqdm(list(folder.glob("*.dax")), desc=f"Checking {folder}"):
+        size = dax.stat().st_size
+        match = re.search("_H([0-9]+)_F_?([0-9]+).dax", str(dax))
+        round = int(match.group(1))
+        fov = int(match.group(2))
+        files.append(ImageFile(round, fov, size))
 
-print(f"INFO: Found {count} images across {len(rounds)} imaging rounds and {len(fovs)} FOVs")
+    if not files:
+        print(f"FAIL: No dax files found in folder. Is it the correct path?")
+        return
 
-nums = set()
-for round in sizes.values():
-    nums.add(len(round))
+    df = pd.DataFrame(files)
 
-if len(nums) == 1:
-    print("PASS: Every image in each round is the same size")
-else:
-    print("FAIL: Some images are different sizes, possible incomplete copy")
+    n_rounds = len(df["round"].unique())
+    n_fovs = len(df["fov"].unique())
+    n_files = len(df)
 
-nums = set()
-for round in sizes.values():
-    nums.add(list(round.values())[0])
+    print(f"INFO: Found images from {n_rounds} imaging rounds and {n_fovs} FOVs")
 
-if len(nums) == 1:
-    print(f"PASS: Every imaging round has {nums.pop()} FOVs")
-else:
-    print("FAIL: Not all imaging rounds have the same number of FOVs, possible missing files")
+    if n_files != n_rounds * n_fovs:
+        print(
+            f"FAIL: Found {n_files} images ({n_rounds} * {n_fovs} = {n_rounds * n_fovs})"
+        )
+        fovs = df["fov"].unique()
+        for hyb in df["round"].unique():
+            hfovs = df[df["round"] == hyb]["fov"].unique()
+            missing = set(fovs) - set(hfovs)
+            if missing:
+                print(f"Hyb {hyb}, FOVs: {missing}")
+    else:
+        print(
+            f"PASS: Found {n_files} images ({n_rounds} * {n_fovs} = {n_rounds * n_fovs})"
+        )
+
+    for round in df.groupby("round")["size"].unique():
+        if len(round) > 1:
+            print("FAIL: Some images in the same round are different sizes")
+            break
+    else:
+        print("PASS: All images in the same round are the same size")
+
+
+if __name__ == "__main__":
+    folder = pathlib.Path(sys.argv[1])
+    verify_folder(folder)
