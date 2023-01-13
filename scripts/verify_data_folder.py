a4dc7088efdd13652f97562ec0089ddcb277991a
Colin Kern <colin.kern@gmail.com>
2023-01-13 15:39:17 -0800

    verify_data_folder.py now works on MERSCOPE data

--- scripts/verify_data_folder.py
+++ scripts/verify_data_folder.py
@@ -2,24 +2,46 @@ import sys
 import pathlib
 import re
 from collections import namedtuple
-from tqdm import tqdm
 import pandas as pd
 
 ImageFile = namedtuple("ImageFile", ["round", "fov", "size"])
 
 
+def parse_image_filename(filename):
+    # Try homebuilt microscope filename
+    match = re.search("_H([0-9]+)_F_?([0-9]+).dax", filename)
+    if match:
+        hyb = int(match.group(1))
+        fov = int(match.group(2))
+        return hyb, fov
+
+    # Try MERSCOPE filename
+    match = re.search("stack_([prestain_0-9]+)_([0-9]+).dax", filename)
+    if match:
+        hyb = match.group(1)
+        fov = int(match.group(2))
+        return hyb, fov
+
+    raise AttributeError(f"Can't determine FOV and imaging round for filename {filename}")
+
+
 def verify_folder(folder):
     files = []
-    for dax in tqdm(list(folder.glob("*.dax")), desc=f"Checking {folder}"):
-        size = dax.stat().st_size
-        match = re.search("_H([0-9]+)_F_?([0-9]+).dax", str(dax))
-        round = int(match.group(1))
-        fov = int(match.group(2))
-        files.append(ImageFile(round, fov, size))
+    print(f"Checking {folder}...")
 
-    if not files:
-        print(f"FAIL: No dax files found in folder. Is it the correct path?")
-        return
+    filenames = list(folder.glob("*.dax"))
+    if not filenames:
+        filenames = list(pathlib.Path(folder / "data").glob("*.dax"))
+        if not filenames:
+            print("FAIL: Could not find any image files. Is it the correct path?")
+            sys.exit(1)
+        fovs = len(pathlib.Path(folder / "settings" / "positions.csv").read_text(encoding="utf-8").split())
+        print(f"Found MERSCOPE positions file with {fovs} FOVs")
+
+    for dax in filenames:
+        size = dax.stat().st_size
+        hyb, fov = parse_image_filename(str(dax))
+        files.append(ImageFile(hyb, fov, size))
 
     df = pd.DataFrame(files)
 
@@ -27,12 +49,10 @@ def verify_folder(folder):
     n_fovs = len(df["fov"].unique())
     n_files = len(df)
 
-    print(f"INFO: Found images from {n_rounds} imaging rounds and {n_fovs} FOVs")
+    print(f"Found images from {n_rounds} imaging rounds and {n_fovs} FOVs")
 
     if n_files != n_rounds * n_fovs:
-        print(
-            f"FAIL: Found {n_files} images ({n_rounds} * {n_fovs} = {n_rounds * n_fovs})"
-        )
+        print(f"FAIL: Found {n_files} images ({n_rounds} * {n_fovs} = {n_rounds * n_fovs})")
         fovs = df["fov"].unique()
         for hyb in df["round"].unique():
             hfovs = df[df["round"] == hyb]["fov"].unique()
@@ -40,12 +60,10 @@ def verify_folder(folder):
             if missing:
                 print(f"Hyb {hyb}, FOVs: {missing}")
     else:
-        print(
-            f"PASS: Found {n_files} images ({n_rounds} * {n_fovs} = {n_rounds * n_fovs})"
-        )
+        print(f"PASS: Found {n_files} images ({n_rounds} * {n_fovs} = {n_rounds * n_fovs})")
 
-    for round in df.groupby("round")["size"].unique():
-        if len(round) > 1:
+    for hyb in df.groupby("round")["size"].unique():
+        if len(hyb) > 1:
             print("FAIL: Some images in the same round are different sizes")
             break
     else:
